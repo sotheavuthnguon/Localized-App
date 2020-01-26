1048aa1a42fd2d8dfd48d3a99fea22211f274145
Sotheavuth Nguon <sotheavuth.nguon@yahoo.com>
2020-01-26 20:19:33 +0700

    Initial Commit

---
+++ Localized App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Localized App
+//
+//  Created by Sotheavuth Nguon on 1/26/20.
+//  Copyright © 2020 Sotheavuth Nguon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
